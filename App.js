7436c26ec97fd736eba7cc12999362e94b6e7796
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-09-14 12:05:59 -0300

    ufrngram layout

---
+++ App.js
@@ -0,0 +1,167 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Constants from 'expo-constants';
+import {FontAwesome5} from '@expo/vector-icons';
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+
+      <View style={styles.header}>
+        <Image style={styles.ufrngram} source={require('./assets/imagens/ufrngram.png')}/>
+        <View style={styles.headerright}>
+          <FontAwesome5 name="plus-square" size={24} color="black"/>
+          <FontAwesome5 name="heart" size={24} color="black"/>
+          <FontAwesome5 name="paper-plane" size={24} color="black"/>
+        </View>
+      </View>
+
+      <View style={styles.stories}>
+        <View style={styles.story}>
+          <Image style={styles.imgstory} source={require('./assets/imagens/gatinho.jpg')}/>
+          <Text> miau </Text>
+        </View>
+        <View style={styles.story}>
+          <Image style={styles.imgstory} source={require('./assets/imagens/gatinho.jpg')}/>
+          <Text> miau </Text>
+        </View>
+        <View style={styles.story}>
+          <Image style={styles.imgstory} source={require('./assets/imagens/gatinho.jpg')}/>
+          <Text> miau </Text>
+        </View>
+        <View style={styles.story}>
+          <Image style={styles.imgstory} source={require('./assets/imagens/gatinho.jpg')}/>
+          <Text> miauauau </Text>
+        </View>
+        <View style={styles.story}>
+          <Image style={styles.imgstory} source={require('./assets/imagens/gatinho.jpg')}/>
+          <Text> miauauau </Text>
+        </View>
+      </View>
+      <View style={styles.feed}>
+        <View style={styles.post}>
+          <View style={styles.postheader}>
+            <View style={styles.postheaderesquerda}>
+              <Image style={styles.postheaderimg} source={require('./assets/imagens/gatinho.jpg')}/>
+              <Text>Miau</Text>
+            </View>
+            <FontAwesome5 name="ellipsis-h" size={18} color="black"/>
+          </View>
+          <Image style={styles.postimg} aspectRatio={813/502} source={require('./assets/imagens/gatinho.jpg')}/>
+          <View style={styles.footer}>
+            <View style={styles.footerleft}>
+              <FontAwesome5 name="heart" size={24} color="black"/>
+              <FontAwesome5 name="comment" size={24} color="black"/>
+              <FontAwesome5 name="paper-plane" size={24} color="black"/>
+            </View>
+            <FontAwesome5 name="bookmark" size={24} color="black"/>
+        </View>
+        <View style={styles.post}>
+          <View style={styles.postheader}>
+            <View style={styles.postheaderesquerda}>
+              <Image style={styles.postheaderimg} source={require('./assets/imagens/gatinho.jpg')}/>
+              <Text>Miau</Text>
+            </View>
+            <FontAwesome5 name="ellipsis-h" size={18} color="black"/>
+          </View>
+          <Image style={styles.postimg} aspectRatio={813/502} source={require('./assets/imagens/gatinho.jpg')}/>
+          <View style={styles.footer}>
+            <View style={styles.footerleft}>
+              <FontAwesome5 name="heart" size={24} color="black"/>
+              <FontAwesome5 name="comment" size={24} color="black"/>
+              <FontAwesome5 name="paper-plane" size={24} color="black"/>
+            </View>
+            <FontAwesome5 name="bookmark" size={24} color="black"/>
+        </View>
+        </View>
+      </View>
+    </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: Constants.statusBarHeight,
+  },
+  header:{
+    flexDirection: 'row',
+    padding: 10,
+    height: 50,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  headerright:{
+    flexDirection: 'row',
+    width: '35%',
+    backgroundColor: 'white',
+    justifyContent: 'space-between',
+  },
+  ufrngram: {
+    height: 25,
+    width: 110,
+  },
+  stories:{
+    flexDirection: 'row',
+    height: 90,
+    backgroundColor: '#00f'
+  },
+  story:{
+    height: 90,
+    width: 90,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  imgstory:{
+    borderRadius:50,
+    width:70,
+    height:70,
+  },
+  feed: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  post:{
+    backgroundColor: 'white',
+  },
+  postheader:{
+    flexDirection: 'row',
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 10,
+  },
+  postheaderesquerda:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    //justifyContent: 'space-between'
+  },
+  postheaderimg:{
+    borderRadius:35,
+    width:30,
+    height:30,
+    marginRight: 10,
+  },
+  postimg:{
+    width: '100%',
+    height: undefined,
+  },
+  footer:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 10,
+    height:50,
+    alignItems: 'center',
+    //backgroundColor: 'pink',
+    backgroundColor: 'white',
+  },
+  footerleft:{
+    flexDirection: 'row',
+    width: '35%',
+    backgroundColor: 'white',
+    justifyContent: 'space-between',
+  }
+});
