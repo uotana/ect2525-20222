111f947110f58209996b27ed8633bce1e6da8ca6
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-09-16 14:36:36 -0300

    implementa FlatList no Feed

--- components/Feed.js
+++ components/Feed.js
@@ -1,50 +1,57 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
 import {FontAwesome5} from '@expo/vector-icons';
-import Constants from 'expo-constants';
 
 export default function Feed(){
-    return(
-        <View style={styles.feed}>
-            <View style={styles.post}>
-                <View style={styles.postheader}>
-                    <View style={styles.postheaderesquerda}>
-                    <Image style={styles.postheaderimg} source={require('../assets/imagens/alfredo.png')}/>
-                    <Text style={styles.postheaderusername}>alfredo</Text>
-                    </View>
-                    <FontAwesome5 name="ellipsis-v" size={16} color="black"/>
-                </View>
-                <Image style={styles.postimg} aspectRatio={813/502} source={require('../assets/imagens/alfredo_e_humana.png')}/>
-                <View style={styles.footer}>
-                    <View style={styles.footerleft}>
-                    <FontAwesome5 name="heart" size={24} color="black"/>
-                    <FontAwesome5 name="comment" size={24} color="black"/>
-                    <FontAwesome5 name="paper-plane" size={24} color="black"/>
-                </View>
-                <FontAwesome5 name="bookmark" size={24} color="black"/>
-            </View>
-            <View style={styles.post}>
-                <View style={styles.postheader}>
-                    <View style={styles.postheaderesquerda}>
-                    <Image style={styles.postheaderimg} source={require('../assets/imagens/cassandra1.png')}/>
-                    <Text style={styles.postheaderusername}>cassandra</Text>
-                </View>
-                <FontAwesome5 name="ellipsis-v" size={16} color="black"/>
-            </View>
-            <Image style={styles.postimg} aspectRatio={813/502} source={require('../assets/imagens/cassandra.png')}/>
-            <View style={styles.footer}>
-                <View style={styles.footerleft}>
-                    <FontAwesome5 name="heart" size={24} color="black"/>
-                    <FontAwesome5 name="comment" size={24} color="black"/>
-                    <FontAwesome5 name="paper-plane" size={24} color="black"/>
-                </View>
-                <FontAwesome5 name="bookmark" size={24} color="black"/>
-            </View>
-            </View>
-            </View>
-        </View>
-    );
-}
 
+    const feed = [
+      {
+        id: 1,
+        nome: "alfredo",
+        srcImgPerfil:require('../assets/imagens/alfredo.png') ,
+        srcImgPost: require('../assets/imagens/alfredo_e_humana.png'),
+        aspectRatio: 813/502
+      },
+      {
+        id: 1,
+        nome: "cassandra",
+        srcImgPerfil:require('../assets/imagens/cassandra1.png') ,
+        srcImgPost: require('../assets/imagens/cassandra.png'),
+        aspectRatio: 813/502
+      },
+    ];
+
+    function renderItem({item}){
+      return <View style={styles.post}>
+      <View style={styles.postheader}>
+          <View style={styles.postheaderesquerda}>
+            <Image style={styles.postheaderimg} source={item.srcImgPerfil}/>
+            <Text style={styles.postheaderusername}>{item.nome}</Text>
+          </View>
+          <FontAwesome5 name="ellipsis-v" size={16} color="black"/>
+      </View>
+      <Image style={styles.postimg} aspectRatio={813/502} source={item.srcImgPost}/>
+      <View style={styles.footer}>
+          <View style={styles.footerleft}>
+            <FontAwesome5 name="heart" size={24} color="black"/>
+            <FontAwesome5 name="comment" size={24} color="black"/>
+            <FontAwesome5 name="paper-plane" size={24} color="black"/>
+        </View>
+        <FontAwesome5 name="bookmark" size={24} color="black"/>
+    </View>
+  </View>
+  }
+  
+
+  return (
+    <View style={styles.feed}>
+      <FlatList
+        data={feed}
+        renderItem = {renderItem}
+        keyExtractor = {item => item.id}
+      />
+    </View>
+  )
+}
 const styles = StyleSheet.create({
     feed: {
       flex: 1,
@@ -93,5 +100,4 @@ const styles = StyleSheet.create({
     postheaderusername:{
       fontWeight: "bold",
     }
-  });
-  
+})
