4a347eb35093543cd68f802394d8f69b068e0682
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-09-29 17:47:50 -0300

    conecta componente Stories com o servidor

--- components/Stories.js
+++ components/Stories.js
@@ -1,39 +1,23 @@
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
 
 export default function Stories() {
 
-  const stories = [
-    {
-      id: 1,
-      nome: 'milena',
-      src: require('../assets/imagens/milena.png'),
-    },
-    {
-      id: 2,
-      nome: 'cassandra',
-      src: require('../assets/imagens/cassandra1.png'),
-    },
-    {
-      id: 3,
-      nome: 'mike',
-      src: require('../assets/imagens/mike.png'),
-    },
-    {
-      id: 4,
-      nome: 'alfredo',
-      src: require('../assets/imagens/alfredo.png'),
-    },
-    {
-      id: 5,
-      nome: 'roberto',
-      src: require('../assets/imagens/roberto.png'),
-    },
-  ];
+  const [stories, setStories] = useState([]);
+
+  useEffect(function(){
+    async function getData(){
+      const response = await fetch('https://mobile.ect.ufrn.br:3000/stories');
+      const stories = await response.json();
+      setStories(stories);
+    }
+    getData();
+  },[])
 
   function renderItem({item}){
     return <View style={styles.story}>
-    <Image style={styles.imgstory} source={item.src}/>
-    <Text> {item.nome} </Text>
+    <Image style={styles.imgstory} source={{uri: item.imgPerfilUri}}/>
+    <Text> {item.nomeUsuario} </Text>
   </View>
   }
 
